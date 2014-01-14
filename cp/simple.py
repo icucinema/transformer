cfcb0f69499fa2dc300edbe1a5a89b8927767c10
Ali Lown <ali@lown.me.uk>
2014-01-14 16:19:47 +0000

    Add a very simple client

---
+++ cp/simple.py
@@ -0,0 +1,49 @@
+from brisa.core.reactors import install_default_reactor
+reactor = install_default_reactor()
+
+from brisa.upnp.control_point import ControlPoint
+from brisa.core.threaded_call import run_async_function
+
+service = 'urn:schemas-upnp-org:service:Audio:1'
+cp500_type = 'urn:schemas-upnp-org:device:CP500:1'
+
+class CP500Controller(ControlPoint):
+  def __init__(self):
+    ControlPoint.__init__(self)
+
+  def volmax(self, device):
+    d = device.get_service_by_type(service)
+    d.SetVolume(99)
+
+  def volmin(self, device):
+    d = device.get_service_by_type(service)
+    d.SetVolume(0)
+
+  def mute(self, device):
+    d = device.get_service_by_type(service)
+    d.SetMute(True)
+
+  def unmute(self, device):
+    d = device.get_service_by_type(service)
+    d.SetMute(False)
+
+  def main(self):
+    commands = {'volmax': self.volmax,
+                'volmin': self.volmin,
+                'mute':   self.mute,
+                'unmute': self.unmute}
+
+    while True:
+      input = raw_input('! ').strip()
+      if input[0] not in commands:
+        print "?"
+      else:
+        commands[input]()
+
+ctl = CP500Controller()
+ctl.start()
+ctl.start_search(2, cp500_type)
+run_async_function(ctl.main)
+
+reactor.add_after_stop_func(ctl.destroy)
+reactor.main()
