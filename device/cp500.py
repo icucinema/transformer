2f8c1833fdd55e77f09df4cb0d6610e3a59cb780
Ali Lown <ali@lown.me.uk>
2014-01-14 16:08:48 +0000

    Add cp500 device + Audio scpd

---
+++ device/cp500.py
@@ -0,0 +1,37 @@
+from brisa.core.reactors import install_default_reactor
+reactor = install_default_reactor()
+import os
+from brisa.upnp.device import Device, Service
+
+class CP500(Device):
+  def __init__(self):
+    Device.__init__(self, 'urn:schemas-upnp-org:device:CP500:1', 'CP500')
+
+class Audio(Service):
+  def __init__(self):
+    Service.__init__(self, 'Audio', 'urn:schemas-upnp-org:service:Audio:1',
+        '', os.getcwd() + '/Audio-scpd.xml')
+    self.volume = 0
+    self.mute = False
+
+    def SetMute(self, *args, **kwargs):
+      self.mute = kwargs['Mute']
+      return {}
+
+    def GetMute(self, *args, **kwargs):
+      return {'Mute': self.mute}
+
+    def SetVolume(self, *args, **kwargs):
+      self.volume = kwargs['Volume']
+      return {}
+
+    def GetVolume(self, *args, **kwargs):
+      return {'Volume': self.volume}
+
+if __name__ == "__main__":
+  device = CP500()
+  device += Audio()
+
+  device.start()
+  reactor.add_after_stop_func(device.stop)
+  reactor.main()
