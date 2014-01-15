1a238fca6915ef7a05b3ed7c5b409ebdfd2adcf7
Ali Lown <ali@lown.me.uk>
2014-01-15 21:54:22 +0000

    Add serial support to cp500 (tested!)

--- device/cp500.py
+++ device/cp500.py
@@ -2,15 +2,61 @@ from brisa.core.reactors._select import *
 reactor = SelectReactor()
 import os
 from brisa.upnp.device import Device, Service
+import serial
+
+#TODO: Bother implementing volume + mute changing when actually necessary$
 
 class CP500(Device):
+  BTN_MAP =  {1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 'Mute': 0xd}
+  VOL_UP =   [0x1, 0x1]
+  VOL_DOWN = [0x1, 0x0]
+  HEADER =   [0x55, 0xaa]
+  SEND =     [0]
+  RECV =     [1]
+  BTN_SEND = [0]
+  BTN_RECV = [8]
+  PKT_LEN =  6
+
   def __init__(self):
     Device.__init__(self, 'urn:schemas-upnp-org:device:CP500:1', 'CP500')
+
+  @staticmethod
+  def makeChecksum(packet):
+    return [0xff - (sum(packet) % 0x100)]
+
+  @staticmethod
+  def makeChecksumPacket(packet):
+    return packet + CP500.makeChecksum(packet)
+
+  @staticmethod
+  def makeButtonPacket(button):
+    return CP500.makeChecksumPacket(CP500.HEADER + CP500.SEND + CP500.BTN_SEND + [CP500.BTN_MAP.get(button, 0xff)])
+
+  @staticmethod
+  def makeVolumeUpPacket():
+    return CP500.makeChecksumPacket(CP500.HEADER + CP500.SEND + CP500.VOL_UP)
+
+  @staticmethod
+  def makeVolumeDownPacket():
+    return CP500.makeChecksumPacket(CP500.HEADER + CP500.SEND + CP500.VOL_DOWN)
+
+  @staticmethod
+  def parseButtonRespPacket(packet):
+    if packet[0,2] != HEADER:
+      raise Exception("invalid header?")
+    if packet[2,4] != [RECV, BTN_RECV]:
+      raise Exception("not a receipt packet")
+    if makeChecksum(packet[0,5]) != packet[5]:
+      raise Exception("checksum mismatch")
+    return packet[4] + 1 #TODO: handle mute button
 
 class Audio(Service):
   def __init__(self):
     Service.__init__(self, 'Audio', 'urn:schemas-upnp-org:service:Audio:1',
         '', os.getcwd() + '/Audio-scpd.xml')
+
+    self.serial = serial.Serial('/dev/ttyUSB0', 9600, timeout=5, bytesize=8, parity='N', stopbits=1)
+
     self.volume = 0
     self.mute = False
     self.source = 0
@@ -30,11 +76,17 @@ class Audio(Service):
     return {'CurrentVolume': self.volume}
 
   def SetSource(self, *args, **kwargs):
-    self.volume = kwargs['NewSource']
+    self.source = int(kwargs['NewSource'])
+    req = CP500.makeButtonPacket(self.source)
+    import binascii
+    print binascii.hexlify(bytearray(req))
+    self.serial.write(bytearray(req))
+    resp = self.serial.read(CP500.PKT_LEN)
+    print binascii.hexlify(resp)
     return {}
 
   def GetSource(self, *args, **kwargs):
-    return {'CurrentSource': self.volume}
+    return {'CurrentSource': self.source}
 
 if __name__ == "__main__":
   device = CP500()
