9f34f931ef7003a824d9ceb4ac6372825d0dc938
Ali Lown <ali@lown.me.uk>
2014-01-23 00:27:47 +0000

    Add screen controller to cp

--- cp/cli.py
+++ cp/cli.py
@@ -4,6 +4,21 @@ reactor = install_default_reactor()
 from brisa.upnp.control_point import ControlPoint
 from brisa.core.threaded_call import run_async_function
 
+def main():
+  commands = {'volmax': cp500_ctl.volmax,
+              'volmin': cp500_ctl.volmin,
+              'mute':   cp500_ctl.mute,
+              'unmute': cp500_ctl.unmute,
+              'raise':  screen_ctl.up,
+              'lower':  screen_ctl.down}
+
+  while True:
+    input = raw_input('! ').strip()
+    if input[0] not in commands:
+      print "?"
+    else:
+      commands[input]()
+
 class CP500Controller(ControlPoint):
   def __init__(self):
     ControlPoint.__init__(self)
@@ -26,23 +41,30 @@ class CP500Controller(ControlPoint):
     d = device.get_service_by_type(self.service)
     d.SetMute(False)
 
-  def main(self):
-    commands = {'volmax': self.volmax,
-                'volmin': self.volmin,
-                'mute':   self.mute,
-                'unmute': self.unmute}
+class ScreenController(ControlPoint):
+  def __init__(self):
+    ControlPoint.__init__(self)
+    self.service = 'urn:schemas-icucinema-co-uk:service:RetractingScreen:1'
+    self.utype = 'urn:schemas-icucinema-co-uk:device:RetractingScreen:1'
 
-    while True:
-      input = raw_input('! ').strip()
-      if input[0] not in commands:
-        print "?"
-      else:
-        commands[input]()
+  def up(self, device):
+    d = device.get_service_by_type(self.service)
+    d.RaiseScreen()
 
-ctl = CP500Controller()
-ctl.start()
-ctl.start_search(2, ctl.utype)
-run_async_function(ctl.main)
+  def down(self, device):
+    d = device.get_service_by_type(self.service)
+    d.LowerScreen()
 
-reactor.add_after_stop_func(ctl.destroy)
+cp500_ctl = CP500Controller()
+cp500_ctl.start()
+cp500_ctl.start_search(2, cp500_ctl.utype)
+reactor.add_after_stop_func(cp500_ctl.destroy)
+
+screen_ctl = ScreenController()
+screen_ctl.start()
+screen_ctl.start_search(2, screen_ctl.utype)
+reactor.add_after_stop_func(screen_ctl.destroy)
+
+run_async_function(main)
+
 reactor.main()
