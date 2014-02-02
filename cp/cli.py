be786e1451344d08e1dba7ceb7b9c3c5243044c3
Ali Lown <ali@lown.me.uk>
2014-02-02 20:32:49 +0000

    Attempt SocketServer

--- cp/cli.py
+++ cp/cli.py
@@ -1,29 +1,31 @@
 from brisa.core.reactors._select import *
 reactor = SelectReactor()
+import SocketServer
 
 from brisa.upnp.control_point import ControlPoint
 from brisa.core.threaded_call import run_async_function
 
-def main():
-  commands = {'vol':    cp500_ctl.setvol,
-              'mute':   cp500_ctl.setmute,
-              'source': cp500_ctl.setsource,
-              'raise':  screen_ctl.up,
-              'lower':  screen_ctl.down,
-              'lights': lx_ctl.set}
+class CommandServer(SocketServer.BaseRequestHandler):
+  def __init__(self):
+    self.commands = {'vol':    cp500_ctl.setvol,
+                     'mute':   cp500_ctl.setmute,
+                     'source': cp500_ctl.setsource,
+                     'raise':  screen_ctl.up,
+                     'lower':  screen_ctl.down,
+                     'lights': lx_ctl.set}
 
-  while True:
-    i = raw_input('! ').strip().split(' ')
-    if i[0] not in commands:
-      if i[0] == "?":
-        print ', '.join(list(commands.viewkeys()))
-      else:
-        print "?"
+  def handle(self):
+    data = self.request[0].strip().split(' ')
+    socket = self.request[1]
+
+    if data[0] not in self.commands:
+      socket.sendto("?", self.client_address)
     else:
       if len(i) == 3:
-        commands[i[0]](i[1], i[2])
+        self.commands[data[0]](data[1], data[2])
       else:
-        commands[i[0]](i[1])
+        self.commands[data[0]](data[1])
+      socket.sendto("?", self.client_address)
 
 class CP500Controller(ControlPoint):
   def __init__(self):
@@ -100,5 +102,7 @@ lx_ctl.start()
 lx_ctl.start_search(2, lx_ctl.utype)
 reactor.add_after_stop_func(lx_ctl.destroy)
 
-run_async_function(main)
-reactor.main()
+if __name__ == "__main__":
+  reactor.main()
+  server = SocketServer.UDPServer(("localhost", 9999), CommandServer)
+  run_async_function(server.serve_forever())
